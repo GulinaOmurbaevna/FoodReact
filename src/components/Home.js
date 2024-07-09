b782572b45be50c72100d99dc9f92ab5744ca5df
chugulina <gulina@MacBook-Air.local>
2024-07-09 18:51:52 +0600

    headerNav

---
+++ src/components/Home.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import Navbar from './Navbar'
+import BannerBackground from '../Assets/home-banner-background.png'
+import BannerImage from '../Assets/home-banner-image.png'
+import { FiArrowRight } from 'react-icons/fi'
+ 
+
+function Home() {
+  return (
+    <div className='home-container'>
+      <Navbar />
+ 
+    </div>
+  )
+}
+
+export default Home
+ 
