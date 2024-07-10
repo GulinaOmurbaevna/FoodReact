f216a20a46ed421a8ee026425806c22b9e1510cb
chugulina <gulina@MacBook-Air.local>
2024-07-10 06:13:37 +0600

    add home

--- src/components/Home.js
+++ src/components/Home.js
@@ -9,7 +9,30 @@ function Home() {
   return (
     <div className='home-container'>
       <Navbar />
- 
+      <div className='home-banner-container'>
+        <div className="home-bannerImage-container">
+          <img src={BannerBackground} alt="" />
+        </div>
+        <div className="home-text-sections">
+          <h1 className='primary-heading'>
+            Your Favorite Food Delivered Hot & Fresh
+          </h1>
+
+          <p className='primary-text'>
+            Healthy switcher chefs do all the prep work , like peeding,
+            chopping
+            & marinating, so you can cook a fresh food
+          </p>
+
+          <button className='secondary-button'>
+               Order Now <FiArrowRight />
+          </button>
+        </div>
+
+        <div className="home-image-container">
+          <img src={BannerImage} alt="" />
+        </div>
+      </div>
     </div>
   )
 }
