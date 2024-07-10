6ebe48526e337df069243c36478b5b03315cda59
chugulina <gulina@MacBook-Air.local>
2024-07-10 17:55:49 +0600

    add footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,49 @@
-import React from 'react'
-
+import React from 'react';
+import Logo from "../Assets/Logo.svg"
+import { BsTwitter } from 'react-icons/bs';
+import { SiLinkedin } from 'react-icons/si';
+import { BsYoutube } from 'react-icons/bs';
+import { FaFacebook } from 'react-icons/fa';
+ 
 function Footer() {
   return (
-    <div>
-      
+    <div className='footer-wrapper'>
+       <div className='footer-section-one'>
+        <div className="footer-logo-container">
+            <img src={Logo} alt="" />
+        </div>
+
+        <div className="footer-icons">
+            <BsTwitter />
+            <SiLinkedin />
+            <BsYoutube />
+            <FaFacebook />
+        </div>
+       </div>
+
+       <div className='footer-section-two'>
+        <div className="footer-section-columns">
+            <span>Quality</span>
+            <span>Help</span>
+            <span>Share</span>
+            <span>Carriers</span>
+            <span>Testimonials</span>
+            <span>Work</span>
+        </div>
+     
+     <div className="footer-section-columns">
+        <span>244-5333-7783</span>
+        <span>Hello@food.com</span>
+        <span>pressFood.com</span>
+        <span>contact@.com</span>
+     </div>
+
+     <div className='footer-section-columns'>
+        <span>Terms & Conditions</span>
+        <span>Privaty policy</span>
+
+     </div>
+       </div>
     </div>
   )
 }
